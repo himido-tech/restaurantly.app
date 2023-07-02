0a8bbbde84cd2f2b9bde88031c9f64439541a6a5
Hiieu <you@example.com>
2023-07-02 10:57:32 +0100

    add navbar as component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,26 +1,25 @@
 export default function NavBar() {
     return (
-      <div className="container">
-        <nav className="navbar navbar-expand-md navbar-light bg-light">
-          <a className="navbar-brand" href="#">Restaurantly</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">QR</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Menu builder</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Profile</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+        <div className="container">
+            <nav className="navbar navbar-expand-md navbar-light bg-light">
+                <a className="navbar-brand" href="#">Restaurantly</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">QR</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/menu">Menu builder</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Profile</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
-  }
-  
+}
