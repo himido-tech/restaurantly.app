8e3c106d3ae88f966f04566fead3c3a9a0599e5f
Hiieu <you@example.com>
2023-07-02 15:15:20 +0100

    fix form layout

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -3,37 +3,33 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  example: string,
-  exampleRequired: string,
+  category: string,
+  description: string,
 };
 
 export default function Menu() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-  console.log(watch("example")) // watch input value by passing the name of it
-
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <main>
       <h1>Menu</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Category</label>
-        <div className="form-group">
+        <div className="d-grid gap-2">
+          <label>Category</label>
           {/* register your input into the hook by invoking the "register" function */}
-          <input placeholder="For example: Main" {...register("example")} />
-        </div>
-        <label>Description</label>
-        <div className="form-group">
+          <input className="w-50" placeholder="For example: Main" {...register("category")} />
+          <label>Description</label>
           {/* include validation with required or other standard HTML validation rules */}
-          <textarea className="w-50"{...register("exampleRequired", { required: true })} placeholder="For example: 
+          <textarea {...register("description", { required: false })} placeholder="For example: 
           Choose from top-rated comfort food, healthy, and vegetarian options. Find your meal now!" />
           {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
-        </div>
+          {errors.category && <span>This field is required</span>}
 
-        <button type="submit" className="btn btn-primary">Add</button>
+          <button type="submit" className="btn btn-primary">Add a section</button>
+        </div>
       </form>
-    </main>
+    </main >
   );
 }
