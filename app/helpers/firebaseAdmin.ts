70caee66801e0fc3ed82cfaa9ce0a08b47d0bbbc
Hiieu <you@example.com>
2023-08-15 09:28:27 +0100

    add a comment

--- app/helpers/firebaseAdmin.ts
+++ app/helpers/firebaseAdmin.ts
@@ -1,11 +1,9 @@
-// I don't know when those instances are spinning up, but there are cases
-// where an instance already exists. It's a good practice to use a single instance.
 import { App, applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app"
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { Adapter } from "next-auth/adapters";
 
+// It's a good practice to use a single instance.
 export var adminApp: App | null = null
-
 if (!getApps().length) {
     adminApp = initializeApp({
         credential: applicationDefault(),
