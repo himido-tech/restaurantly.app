5c9c61a0e93578a45c1178f41a2a4f70dfb3dcaf
Hiieu <you@example.com>
2023-08-16 23:07:54 +0100

    fix failing pipeline

--- app/helpers/firebase.ts
+++ app/helpers/firebase.ts
@@ -1,19 +1,15 @@
 // The Firebase Admin SDK can be used in the backend to manage users.
 import { App, applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app"
+import admin from "firebase-admin"
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { Adapter } from "next-auth/adapters";
-import { fs } from 'memfs';
 
 // It's a good practice to use a single instance.
 export var adminApp: App | null = null
 if (!getApps().length) {
     const config = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG || "")
-    // Set this environment variable before deployment: 
-    // GOOGLE_APPLICATION_CREDENTIALS=/firebase-admin-creds.json
-    fs.writeFileSync('/firebase-admin-creds.json', config);
-
     adminApp = initializeApp({
-        credential: applicationDefault(),
+        credential: admin.credential.cert(config),
     })
 } else {
     adminApp = getApp()
