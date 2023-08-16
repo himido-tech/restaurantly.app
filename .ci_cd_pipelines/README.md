119e5cad4dfb46e4b5088f6cd49eecf249fadd88
Hiieu <you@example.com>
2023-08-16 22:34:54 +0100

    configure firebase admin for heroku

---
+++ .ci_cd_pipelines/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+This folder contains ci/cd pipeline for the project. It is based on Heroku.
--- app/helpers/firebase.ts
+++ app/helpers/firebase.ts
@@ -2,10 +2,16 @@
 import { App, applicationDefault, getApp, getApps, initializeApp } from "firebase-admin/app"
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { Adapter } from "next-auth/adapters";
+import { fs } from 'memfs';
 
 // It's a good practice to use a single instance.
 export var adminApp: App | null = null
 if (!getApps().length) {
+    const config = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG || "")
+    // Set this environment variable before deployment: 
+    // GOOGLE_APPLICATION_CREDENTIALS=/firebase-admin-creds.json
+    fs.writeFileSync('/firebase-admin-creds.json', config);
+
     adminApp = initializeApp({
         credential: applicationDefault(),
     })
