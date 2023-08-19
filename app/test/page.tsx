0c4294c808bb98889866ab8d0fa10187ce4ff04e
Hiieu <you@example.com>
2023-08-19 14:21:20 +0100

    improve navbar

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -149,17 +149,27 @@ export default function PersistentDrawerLeft() {
     const user = session?.user
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
 
     const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
     const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleMenuClose = () => {
-        signOut();
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
     };
-    // Account menu
+
+    const handleMenuClose = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    };
+
+    const handleLogout = () => {
+        signOut()
+    };
+    // Account menu (desktop)
     const menuId = 'primary-search-account-menu';
     const renderMenu = (
         <Menu
@@ -178,7 +188,59 @@ export default function PersistentDrawerLeft() {
             onClose={handleMenuClose}
         >
             <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+    );
+
+    // Account menu (mobile)
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon />
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+            </MenuItem>
         </Menu>
     );
 
@@ -340,6 +402,10 @@ export default function PersistentDrawerLeft() {
                     eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
                     posuere sollicitudin aliquam ultrices sagittis orci a.
                 </Typography>
+                {
+                    (sessionStatus === "unauthenticated") ? null : renderMobileMenu
+                }
+                {renderMenu}
             </Main>
         </Box >
     );
